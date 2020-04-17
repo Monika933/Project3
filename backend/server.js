cf7c1cac7440daa5b40cea00a7486989a119def6
Tina Karanovic <tinakaranovic7@gmail.com>
2020-04-16 20:10:28 -0400

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
-require('dotenv').config();
-
+const routes = require('./routes');
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
+var databaseToUse = ""
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static('client/build'));
+	databaseToUse = "mongodb://project3:Filipo21@ds137488.mlab.com:37488/heroku_99hlg18r";
+}
+else {
+	databaseToUse = 'mongodb://localhost/reactBoilerplate';
+}
+
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
