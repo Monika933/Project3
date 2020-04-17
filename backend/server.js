23dc189d75fd82d3dd5eaa7ca3597e268d4ef325
Tina Karanovic <tinakaranovic7@gmail.com>
2020-04-16 20:18:45 -0400

    sserver.js change

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const routes = require('./routes');
+
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
-var databaseToUse = ""
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static('client/build'));
-	databaseToUse = "mongodb://project3:Filipo21@ds137488.mlab.com:37488/heroku_99hlg18r";
-}
-else {
-	databaseToUse = 'mongodb://localhost/reactBoilerplate';
-}
-
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
